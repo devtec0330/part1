098f2dd236187fdb8962bdfae4f216bf7361ed11
devtec <devtec0330@outlook.com>
2024-09-25 17:08:09 -0300

    hoje

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       bem vindo ao react feito por mim
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
